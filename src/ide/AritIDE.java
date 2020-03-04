f2c80a4f6a8404c7a2f58ed2a46f0fe601f4a6a0
danii_mor <daniiel.morales@outlook.com>
2020-03-03 23:09:13 -0600

    add execute code and show dot script
    update name of saveto jbutton

--- src/ide/AritIDE.java
+++ src/ide/AritIDE.java
@@ -26,169 +26,182 @@ import java.awt.event.MouseEvent;
 
 public class AritIDE {
 
-    public JFrame frmAritIDE;
-    private ArrayList<String> rutas = new ArrayList<>();
-    private JTabbedPane jTabs;
+	public JFrame frmAritIDE;
+	private ArrayList<String> rutas = new ArrayList<>();
+	private JTabbedPane jTabs;
 
-    /**
-     * danii_mor
-     */
-    public static void main(String[] args) {
-	EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		try {
-		    AritIDE window = new AritIDE();
-		    window.frmAritIDE.setVisible(true);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
-	    }
-	});
-    }
+	/**
+	 * danii_mor
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AritIDE window = new AritIDE();
+					window.frmAritIDE.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    /**
-     * Create the application.
-     */
-    public AritIDE() {
-	initialize();
-    }
+	/**
+	 * Create the application.
+	 */
+	public AritIDE() {
+		initialize();
+	}
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-	frmAritIDE = new JFrame();
-	frmAritIDE.setMinimumSize(new Dimension(250, 300));
-	frmAritIDE
-		.setIconImage(Toolkit.getDefaultToolkit().getImage(AritIDE.class.getResource("/ide/iconos/boot.png")));
-	frmAritIDE.setTitle("AritIDE - danii_mor");
-	frmAritIDE.setBounds(100, 100, 450, 300);
-	frmAritIDE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmAritIDE = new JFrame();
+		frmAritIDE.setMinimumSize(new Dimension(250, 300));
+		frmAritIDE
+				.setIconImage(Toolkit.getDefaultToolkit().getImage(AritIDE.class.getResource("/ide/iconos/boot.png")));
+		frmAritIDE.setTitle("AritIDE - danii_mor");
+		frmAritIDE.setBounds(100, 100, 450, 300);
+		frmAritIDE.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	JMenuBar menuBar = new JMenuBar();
-	frmAritIDE.setJMenuBar(menuBar);
+		JMenuBar menuBar = new JMenuBar();
+		frmAritIDE.setJMenuBar(menuBar);
 
-	JMenu mnFile = new JMenu("Archivo");
-	menuBar.add(mnFile);
+		JMenu mnFile = new JMenu("Archivo");
+		menuBar.add(mnFile);
 
-	JPanel addTab = new JPanel();
+		JPanel addTab = new JPanel();
 
-	JTextArea terminal_txt = new JTextArea();
-	terminal_txt.setBackground(Color.BLACK);
-	terminal_txt.setForeground(Color.WHITE);
-	terminal_txt.append(">> Bienvenido a AritIDE - danii_mor \n");
-	terminal_txt.setEditable(false);
-	javax.swing.JScrollPane terminal = new javax.swing.JScrollPane(terminal_txt);
+		JTextArea terminal_txt = new JTextArea();
+		terminal_txt.setBackground(Color.BLACK);
+		terminal_txt.setForeground(Color.WHITE);
+		terminal_txt.append(">> Bienvenido a AritIDE - danii_mor \n");
+		terminal_txt.setEditable(false);
+		javax.swing.JScrollPane terminal = new javax.swing.JScrollPane(terminal_txt);
 
-	jTabs = new JTabbedPane(JTabbedPane.TOP);
+		jTabs = new JTabbedPane(JTabbedPane.TOP);
 
-	jTabs.addTab("+", addTab);
-	addTab.getAccessibleContext().setAccessibleName("");
+		jTabs.addTab("+", addTab);
+		addTab.getAccessibleContext().setAccessibleName("");
 
-	rutas.add("");
-	jTabs.add(new EditorText(), "Archivo1", 0);
-	jTabs.setSelectedIndex(0);
+		rutas.add("");
+		jTabs.add(new EditorText(), "Archivo1", 0);
+		jTabs.setSelectedIndex(0);
 
-	jTabs.addMouseListener(new java.awt.event.MouseAdapter() {
-	    public void mouseClicked(java.awt.event.MouseEvent evt) {
-		int selectedTab = jTabs.getSelectedIndex();
-		if ((jTabs.getTabCount() - 1) == selectedTab) {
-		    rutas.add("");
+		jTabs.addMouseListener(new java.awt.event.MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent evt) {
+				int selectedTab = jTabs.getSelectedIndex();
+				if ((jTabs.getTabCount() - 1) == selectedTab) {
+					rutas.add("");
 
-		    jTabs.add(new EditorText(), "Archivo" + (selectedTab + 1), selectedTab);
-		    jTabs.setSelectedIndex(selectedTab);
-		}
-	    }
-	});
+					jTabs.add(new EditorText(), "Archivo" + (selectedTab + 1), selectedTab);
+					jTabs.setSelectedIndex(selectedTab);
+				}
+			}
+		});
 
-	JMenuItem open = new JMenuItem("Abrir");
-	open.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/document-open.png")));
-	mnFile.add(open);
-	open.addActionListener(new ActionListener() {
+		JMenuItem open = new JMenuItem("Abrir");
+		open.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/document-open.png")));
+		mnFile.add(open);
+		open.addActionListener(new ActionListener() {
 
-	    @Override
-	    public void actionPerformed(ActionEvent arg0) {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
 
-		// Codigo para abrir archivo
+				// Codigo para abrir archivo
 
-		JFileChooser open = new JFileChooser();
-		open.setDialogTitle("AritIDE - danii_mor");
+				JFileChooser open = new JFileChooser();
+				open.setDialogTitle("AritIDE - danii_mor");
 
-		// open.setFileFilter(new FileNameExtensionFilter("Seleccionar archivo",
-		// ".arit"));
+				// open.setFileFilter(new FileNameExtensionFilter("Seleccionar archivo",
+				// ".arit"));
 
-		if (open.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-		    try {
+				if (open.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					try {
 
-			int position = jTabs.getComponentCount() - 1;
+						int position = jTabs.getComponentCount() - 1;
 
-			rutas.add(open.getSelectedFile().getCanonicalPath());
+						rutas.add(open.getSelectedFile().getCanonicalPath());
 
-			jTabs.add(new EditorText("", open.getSelectedFile()), open.getSelectedFile().getName(),
-				position);
-			jTabs.setSelectedIndex(position);
+						jTabs.add(new EditorText("", open.getSelectedFile()), open.getSelectedFile().getName(),
+								position);
+						jTabs.setSelectedIndex(position);
 
-		    } catch (IOException e) {
-			terminal_txt.append(String.valueOf(e));
-		    }
-		}
-	    }
+					} catch (IOException e) {
+						terminal_txt.append(String.valueOf(e));
+					}
+				}
+			}
 
-	});
+		});
 
-	JMenuItem guardar = new JMenuItem("Guardar");
-	guardar.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/kazam-countdown.png")));
-	mnFile.add(guardar);
-	guardar.addActionListener(new ActionListener() {
+		JMenuItem guardar = new JMenuItem("Guardar");
+		guardar.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/kazam-countdown.png")));
+		mnFile.add(guardar);
+		guardar.addActionListener(new ActionListener() {
 
-	    // Codigo para guardar
+			// Codigo para guardar
 
-	    @Override
-	    public void actionPerformed(ActionEvent arg0) {
-		String file = rutas.get(jTabs.getSelectedIndex());
-		if (file.equals("")) {
-		    // Se envia a guardar como
-		    terminal_txt.append(
-			    saveLike(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText()));
-		} else {
-		    try (java.io.FileWriter fw = new java.io.FileWriter(file)) {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String file = rutas.get(jTabs.getSelectedIndex());
+				if (file.equals("")) {
+					// Se envia a guardar como
+					terminal_txt.append(
+							saveLike(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText()));
+				} else {
+					try (java.io.FileWriter fw = new java.io.FileWriter(file)) {
 
-			fw.write(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText());
-			fw.close();
+						fw.write(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText());
+						fw.close();
 
-		    } catch (IOException e) {
-			terminal_txt.append(String.valueOf(e) + "\n");
-		    }
-		}
-	    }
+					} catch (IOException e) {
+						terminal_txt.append(String.valueOf(e) + "\n");
+					}
+				}
+			}
 
-	});
+		});
 
-	JMenuItem mntmProyecto_2 = new JMenuItem("Guardar como");
-	mntmProyecto_2.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/kazam-recording.png")));
-	mnFile.add(mntmProyecto_2);
-	mntmProyecto_2.addActionListener(new ActionListener() {
+		JMenuItem guardarComo = new JMenuItem("Guardar como");
+		guardarComo.setIcon(new ImageIcon(AritIDE.class.getResource("/ide/iconos/kazam-recording.png")));
+		mnFile.add(guardarComo);
+		guardarComo.addActionListener(new ActionListener() {
 
-	    @Override
-	    public void actionPerformed(ActionEvent arg0) {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
 
-		terminal_txt.append(
-			saveLike(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText()));
+				terminal_txt.append(
+						saveLike(((EditorText) jTabs.getComponent(jTabs.getSelectedIndex() + 1)).txt.getText()));
 
-	    }
-	});
+			}
+		});
 
-	JMenu mnRun = new JMenu("Analisis");
-	menuBar.add(mnRun);
+		JMenu mnRun = new JMenu("Analisis");
+		menuBar.add(mnRun);
 
-	JMenuItem ejecutar = new JMenuItem("Ejecutar");
-	ejecutar.setIcon(new javax.swing.ImageIcon(AritIDE.class.getResource("/ide/iconos/kt-start.png"))); // NOI18N
-	ejecutar.setText("Ejecutar");
-	ejecutar.addActionListener(new ActionListener() {
+		JMenuItem ejecutar = new JMenuItem("Ejecutar");
+		ejecutar.setIcon(new javax.swing.ImageIcon(AritIDE.class.getResource("/ide/iconos/kt-start.png"))); // NOI18N
+		ejecutar.setText("Ejecutar");
+		ejecutar.addActionListener(new ActionListener() {
 
-	    @Override
-	    public void actionPerformed(ActionEvent arg0) {
-		// Codigo para ejecutar
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// Codigo para ejecutar
+
+				java.io.Reader reader = new java.io.StringReader(
+						((EditorText) jTabs.getComponentAt(jTabs.getSelectedIndex())).txt.getText());
+				syntax.LexicoAscendente lexer = new syntax.LexicoAscendente(reader);
+				syntax.SintacticoAscendente parser = new syntax.SintacticoAscendente(lexer);
+
+				try {
+					parser.parse();
+					new ast.Graficador(parser.root);
+				} catch (Exception e) {
+					// Auto-generated catch block
+					e.printStackTrace();
+				}
 	    }
 	});
 	mnRun.add(ejecutar);

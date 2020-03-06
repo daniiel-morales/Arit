b3d7b45ee56033f9d0b2d53923e7476bde656fcd
danii_mor <daniiel.morales@outlook.com>
2020-03-05 22:43:42 -0600

    update interface method value
    execute can be used as value method in expresion node

--- src/ast/NodoAST.java
+++ src/ast/NodoAST.java
@@ -1,12 +1,38 @@
 package ast;
 
 public interface NodoAST {
-	public void add(NodoAST n);
-	public Object execute(NodoAST n);
-	public void value(Object lex);
-	public void type(char c);
+	public void add(Object o);
+	public Object execute(sym_table.Tabla_Instancias ambito);
+	public void type(int t);
 	public Object getValue();
-	public char getType();
+	public int getType();
 	public NodoAST getChild(int i);
 	public int getSize();
+	public enum TYPE{
+		//leaf
+		ID, 
+		NUM, 
+		CHAR,
+		BOOLEAN, 
+		STRING,
+		NULL,
+		ERROR, 
+		BREAK,
+		//root
+		SWITCH,
+		CASE,
+		DEFAULT,
+		DECLARE,
+		IF, 
+		ELSE, 
+		IF_ELSE,
+		PRINT,
+		ADD,
+		SUB,
+		MUL,
+		DIV,
+		POT,
+		MOD,
+		SCOPE
+	}
 }
